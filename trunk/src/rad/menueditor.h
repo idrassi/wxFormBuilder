bc9cdb5e0485e8a22b7487d29f675d481394a23d
ja_ortega <ja_ortega@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2005-06-19 09:38:44 +0000

    *Comment menueditor.h

--- trunk/src/rad/menueditor.h
+++ trunk/src/rad/menueditor.h
@@ -34,20 +34,53 @@
 class MenuEditor : public wxDialog
 {
   private:
+    /** Devuelve el índice del elemento seleccionado en el wxListCtrl */
     long GetSelectedItem();
+    
+    /** Devuelve la profundidad del elemento "n" del wxListCtrl */
     int GetItemIdentation(long n);
+    
+    /** Inserta en la posición "n" un elemento en el wxListCtrl. No añade
+    sangría en el campo label. */
     long InsertItem(long n, const wxString& label, const wxString& shortcut,
         const wxString& id, const wxString& name, const wxString& helpString,
         const wxString& kind);
+        
+    /** Inserta debajo del elemento seleccionado (o en la última posición en
+    caso de no haber selección) un nuevo elemento en el wxListCtrl. El campo
+    label se sangra al mismo nivel que el elemento que le precede */
     void AddItem(const wxString& label, const wxString& shortcut, 
         const wxString& id, const wxString& name, const wxString &help,
         const wxString& kind);
+        
+    /** Determina el índice en el que acaba el menú que empieza en la posición
+    "n" */
     long GetEndIndex(long n);
+    
+    /** Devuelve los campos del wxListCtrl del elemento "n". El campo label
+    lo devuelve con sangría */
     void GetItem(long n, wxString& label, wxString& shortcut, wxString& id, 
         wxString& name, wxString& help, wxString& kind);
+        
+    /** Inserta en la posición "n" del wxListCtrl, los hijos de "obj". El primer
+    hijo de "obj" estará sangrado con el valor de "ident". En "n" se devuelve
+    la siguiente posición libre del wxListCtrl tras hacer la inserción */
     void AddChild(long& n, int ident, PObjectBase obj);
+    
+    /** Determina si el elemento "n" del wxListCtrl tiene submenús o por el 
+    contrario es un menuitem */
     bool HasChildren(long n);
+    
+    /** Devuelve el ObjectBase correspondiente al menú que empieza en la posición
+    "n" del wxListCtrl, así como todos los hijos de ese menú. En "n" devuelve
+    la posición en el wxListCtrl del siguiente menú. isSubMenu debe ser true
+    para los menús de primer nivel (aquellos que aparecerán en la barra de menús)
+    y false para el resto (submenús dentro de los menús). */
     PObjectBase GetMenu(long& n, PObjectDatabase base, bool isSubMenu = true);
+    
+    /** Toma los valores introducidos por el usuario en los campos e inserta
+    el elemento en el wxListCtrl. Tras la inserción, vacía todos los campos
+    y pone el foco en el campo "label". */
     void AddNewItem();
   
   protected:
@@ -63,9 +96,19 @@ class MenuEditor : public wxDialog
   
   public:
     
+    /** Constructor */
     MenuEditor(wxWindow *parent, int id = -1);
+    
+    /** Rellena el wxListCtrl con los datos de "obj", que debe ser de tipo
+    menubar */
     void Populate(PObjectBase obj);
+    
+    /** Devuelve la jerarquía de ObjectBase correspondiente a la barra de 
+    menús que hay en el wxListCtrl. El objeto devuelto será, por tanto, de
+    tipo wxMenuBar */
     PObjectBase GetMenubar(PObjectDatabase base);
+    
+    /** Eventos de la interfaz de usuario */
     void OnAddMenuItem(wxCommandEvent& e);
     void OnAddSeparator(wxCommandEvent& e);
     void OnRemoveMenuItem(wxCommandEvent& e);
