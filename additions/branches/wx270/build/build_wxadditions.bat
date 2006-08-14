5cf78107d4899d2bf8c0eb7402568935f8817f3c
rjpcomputing <rjpcomputing@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2006-08-14 18:58:47 +0000

    VC should now build with bakefile. I tried to work with BakeFile for Gcc. It is a work in progress. Use Code::Blocks until the bakefile is fixed. Made a great batch file for building projects. It is standard so every BakeFile/wxBuild_default.bat combination should just work.

--- additions/branches/wx270/build/build_wxadditions.bat
+++ additions/branches/wx270/build/build_wxadditions.bat
@@ -16,31 +16,31 @@ call bakefile_gen
 echo Building awx with VC7.1
 echo.
 cd awx
-call wxBuild_default-vc71.bat ALL
+call wxBuild_default VCTK ALL
 cd ..
 
 echo Building plot with VC7.1
 echo.
 cd plot
-call wxBuild_default-vc71.bat ALL
+call wxBuild_default VCTK ALL
 cd ..
 
 echo Building propgrid with VC7.1
 echo.
 cd propgrid
-call wxBuild_default-vc71.bat ALL
+call wxBuild_default VCTK ALL
 cd ..
 
 echo Building wxFlatNotebook with VC7.1
 echo.
 cd wxFlatNotebook
-call wxBuild_default-vc71.bat ALL
+call wxBuild_default VCTK ALL
 cd ..
 
 echo Building wxScintilla with VC7.1
 echo.
 cd wxScintilla
-call wxBuild_default-vc71.bat ALL
+call wxBuild_default VCTK ALL
 cd ..
 
 echo Done building wxAdditions.
