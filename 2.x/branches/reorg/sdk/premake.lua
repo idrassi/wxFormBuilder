988385a2a9f3730ee8150a9419a2337116710254
rjpcomputing <rjpcomputing@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2006-10-03 19:06:28 +0000

    Initial branch, big project level change. Moving plugins out of src directory.

---
+++ 2.x/branches/reorg/sdk/premake.lua
@@ -0,0 +1,9 @@
+project.name = "wxFormBuilder SDK"
+if ( OS == "windows" ) then
+	project.configs = { "Debug", "Debug (Unicode)", "Release", "Release (Unicode)" }
+end
+project.libdir = "lib"
+
+-- Add sdk projects here.
+dopackage( "plugin_interface" )
+dopackage( "tinyxml" )
