38a68ecab410c288f504fb457be76b47fd684a49
rjpcomputing <rjpcomputing@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2006-10-10 23:02:48 +0000

    Cleanup of unused vc and C::B projects. Many updates to the premake.lua scripts. They are still not functional, but getting closer. Remoced TODO.txt because of the issue tracker.

--- additions/trunk/build/propgrid/premake.lua
+++ additions/trunk/build/propgrid/premake.lua
@@ -1,11 +1,9 @@
-if ( OS == "windows") then
-	package.name = "Plugin Interface"
-else
-	package.name = "plugin_interface-unix"
-end
+package.name = "wxPropGrid"
+
 package.kind = "dll"
+--package.kind = "lib"
 package.language = "c++"
-package.files = { matchfiles( "*.h", "*.cpp" ) }
+package.files = { matchfiles( "../../src/propgrid/*.cpp" ) }
 
 -- Set object output directory.
 package.config["Debug"].objdir = ".objsd"
@@ -14,10 +12,17 @@ package.config["Release"].objdir = ".objs"
 package.config["Release (Unicode)"].objdir = ".objsu"
 
 -- Set the targets.
-package.config["Debug"].target = "wxmsw270md_propgrid_gcc"
-package.config["Debug (Unicode)"].target = "wxmsw270umd_propgrid_gcc"
-package.config["Release"].target = "wxmsw270m_propgrid_gcc"
-package.config["Release (Unicode)"].target = "wxmsw270um_propgrid_gcc"
+if ( options[target] == "cb-gcc" or options[target] == "gnu" ) then
+	package.config["Debug"].target = "wxmsw270md_scintilla_gcc"
+	package.config["Debug (Unicode)"].target = "wxmsw270umd_scintilla_gcc"
+	package.config["Release"].target = "wxmsw270m_scintilla_gcc"
+	package.config["Release (Unicode)"].target = "wxmsw270um_scintilla_gcc"
+else
+	package.config["Debug"].target = "wxmsw270md_scintilla_vc"
+	package.config["Debug (Unicode)"].target = "wxmsw270umd_scintilla_vc"
+	package.config["Release"].target = "wxmsw270m_scintilla_vc"
+	package.config["Release (Unicode)"].target = "wxmsw270um_scintilla_vc"
+end
 
 -- Set the build options for the Unicode build Targets.
 package.buildflags = { "extra-warnings" }
@@ -26,20 +31,36 @@ package.config["Release"].buildflags = { "no-symbols", "optimize-speed" }
 package.config["Release (Unicode)"].buildflags = { "unicode", "no-symbols", "optimize-speed" }
 
 -- Set include paths
-package.includepaths = { "$(WXWIN)/include", "../tinyxml" }
+package.includepaths = { "../../include", "$(WXWIN)/include" }
 
--- Set the link includes
-package.libs = {}
+-- Setup the linker options.
+if ( options[target] == "cb-gcc" or options[target] == "gnu" ) then
+	package.libpaths = { "$(WXWIN)/lib/gcc_dll" }
+else
+	package.libpaths = { "$(WXWIN)/lib/vc_dll" }
+end
+
+-- Setup the output directory options.
+if ( options[target] == "cb-gcc" or options[target] == "gnu" ) then
+	package.bindir = { "../lib/gcc_dll" }
+else
+	package.bindir = { "../lib/vc_dll" }
+end
+
+package.config["Debug"].links = { "wxmsw27d" }
+package.config["Debug (Unicode)"].links = { "wxmsw27ud" }
+package.config["Release"].links = { "wxmsw27" }
+package.config["Release (Unicode)"].links = { "wxmsw27u" }
 
 -- Set defines.
 if ( OS == "windows") then
-	package.config["Debug"].defines = { "DEBUG", "WIN32", "_WINDOWS", "HAVE_W32API_H", "__WX__", "__WXMSW__", "__WXDEBUG__", "TIXML_USE_TICPP", "WXUSINGDLL" }
-	package.config["Debug (Unicode)"].defines = { "DEBUG", "WIN32", "_WINDOWS", "HAVE_W32API_H", "__WX__", "__WXMSW__", "__WXDEBUG__", "TIXML_USE_TICPP", "UNICODE", "_UNICODE", "WXUSINGDLL" }
-	package.config["Release"].defines = { "NDEBUG", "WIN32", "_WINDOWS", "HAVE_W32API_H", "__WX__", "__WXMSW__", "TIXML_USE_TICPP", "WXUSINGDLL" }
-	package.config["Release (Unicode)"].defines = { "NDEBUG", "WIN32", "_WINDOWS", "HAVE_W32API_H", "__WX__", "__WXMSW__", "TIXML_USE_TICPP", "UNICODE", "_UNICODE", "WXUSINGDLL" }
+	package.config["Debug"].defines = { "WXMAKINGDLL_PROPGRID", "MONOLITHIC", "DEBUG", "WIN32", "_WINDOWS", "HAVE_W32API_H", "__WX__", "__WXMSW__", "__WXDEBUG__", "WXUSINGDLL" }
+	package.config["Debug (Unicode)"].defines = { "WXMAKINGDLL_PROPGRID", "MONOLITHIC", "DEBUG", "WIN32", "_WINDOWS", "HAVE_W32API_H", "__WX__", "__WXMSW__", "__WXDEBUG__", "UNICODE", "_UNICODE", "WXUSINGDLL" }
+	package.config["Release"].defines = { "WXMAKINGDLL_PROPGRID", "MONOLITHIC", "NDEBUG", "WIN32", "_WINDOWS", "HAVE_W32API_H", "__WX__", "__WXMSW__", "TIXML_USE_TICPP", "WXUSINGDLL" }
+	package.config["Release (Unicode)"].defines = { "WXMAKINGDLL_PROPGRID", "MONOLITHIC", "NDEBUG", "WIN32", "_WINDOWS", "HAVE_W32API_H", "__WX__", "__WXMSW__", "UNICODE", "_UNICODE", "WXUSINGDLL" }
 else
-	package.config["Debug"].defines = { "DEBUG", "__WX__", "__WXDEBUG__", "TIXML_USE_TICPP", "WXUSINGDLL" }
-	package.config["Release"].defines = { "NDEBUG", "__WX__", "TIXML_USE_TICPP", "WXUSINGDLL" }
+	package.config["Debug"].defines = { "WXMAKINGDLL_PROPGRID", "MONOLITHIC", "DEBUG", "__WX__", "__WXDEBUG__", "WXUSINGDLL" }
+	package.config["Release"].defines = { "WXMAKINGDLL_PROPGRID", "MONOLITHIC", "NDEBUG", "__WX__", "WXUSINGDLL" }
 end
 
 -- Set build optionsfor Linux.
