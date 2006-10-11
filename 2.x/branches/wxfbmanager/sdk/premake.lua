cb29ab8585430d22e5fe9b28ae7c4b3e41549f8f
rjmyst3 <rjmyst3@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2006-10-11 14:59:23 +0000

    Removed file/folder

--- 2.x/branches/wxfbmanager/sdk/premake.lua
+++
@@ -1,7 +0,0 @@
-project.name = "wxFormBuilder SDK"
-project.configs = { "Debug", "Debug (Unicode)", "Release", "Release (Unicode)" }
-project.libdir = "lib"
-
--- Add sdk projects here.
-dopackage( "Plugin_Interface" )
-dopackage( "TinyXML" )
