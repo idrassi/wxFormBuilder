242e4f25abfc9fa8146158dc6b0d01810baa8ee0
rjpcomputing <rjpcomputing@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2006-12-13 22:25:44 +0000

    Updated premake scripts with needed Linux fixes.

--- additions/trunk/build/wxFlatNotebook/premake.lua
+++ additions/trunk/build/wxFlatNotebook/premake.lua
@@ -4,6 +4,10 @@ package.kind = "dll"
 package.language = "c++"
 package.files = { matchfiles( "../../src/wxFlatNotebook/*.cpp", "../../include/wx/wxFlatNotebook/*.h" ) }
 
+-- Local variables
+local wx_ver = "27"
+local wx_ver_minor = "0"
+
 -- Set object output directory.
 package.config["Debug"].objdir = ".objsd"
 package.config["Debug (Unicode)"].objdir = ".objsud"
@@ -13,15 +17,15 @@ package.config["Release (Unicode)"].objdir = ".objsu"
 -- Set the targets.
 if ( OS == "windows") then
 	if ( target == "cb-gcc" or target == "gnu" ) then
-		package.config["Debug"].target = "wxmsw270md_flatnotebook_gcc"
-		package.config["Debug (Unicode)"].target = "wxmsw270umd_flatnotebook_gcc"
-		package.config["Release"].target = "wxmsw270m_flatnotebook_gcc"
-		package.config["Release (Unicode)"].target = "wxmsw270um_flatnotebook_gcc"
+		package.config["Debug"].target = "wxmsw"..wx_ver..wx_ver_minor.."md_flatnotebook_gcc"
+		package.config["Debug (Unicode)"].target = "wxmsw"..wx_ver..wx_ver_minor.."umd_flatnotebook_gcc"
+		package.config["Release"].target = "wxmsw"..wx_ver..wx_ver_minor.."m_flatnotebook_gcc"
+		package.config["Release (Unicode)"].target = "wxmsw"..wx_ver..wx_ver_minor.."um_flatnotebook_gcc"
 	else
-		package.config["Debug"].target = "wxmsw270md_flatnotebook_vc"
-		package.config["Debug (Unicode)"].target = "wxmsw270umd_flatnotebook_vc"
-		package.config["Release"].target = "wxmsw270m_flatnotebook_vc"
-		package.config["Release (Unicode)"].target = "wxmsw270um_flatnotebook_vc"
+		package.config["Debug"].target = "wxmsw"..wx_ver..wx_ver_minor.."md_flatnotebook_vc"
+		package.config["Debug (Unicode)"].target = "wxmsw"..wx_ver..wx_ver_minor.."umd_flatnotebook_vc"
+		package.config["Release"].target = "wxmsw"..wx_ver..wx_ver_minor.."m_flatnotebook_vc"
+		package.config["Release (Unicode)"].target = "wxmsw"..wx_ver..wx_ver_minor.."um_flatnotebook_vc"
 	end
 else
 	package.config["Debug"].target = "`wx-config --debug --basename`_flatnotebook-`wx-config --release`"
@@ -35,11 +39,23 @@ package.config["Release"].buildflags = { "no-symbols", "optimize-speed" }
 package.config["Release (Unicode)"].buildflags = { "unicode", "no-symbols", "optimize-speed" }
 
 -- Set include paths
-package.includepaths = { "../../include", "$(WXWIN)/include" }
+if ( OS == "windows") then
+	if ( target == "cb-gcc" ) then
+		package.includepaths = { "../../include", "$(#WX.include)" }
+	else
+		package.includepaths = { "../../include", "$(WXWIN)/include" }
+	end
+else
+	package.includepaths = { "../../include" }
+end
 
 -- Setup the linker options.
 if ( target == "cb-gcc" or target == "gnu" ) then
-	package.libpaths = { "$(WXWIN)/lib/gcc_dll" }
+	if ( target == "cb-gcc" and OS == "windows" ) then
+		package.libpaths = { "$(#WX.lib)/gcc_dll" }
+	else
+		package.libpaths = { "$(WXWIN)/lib/gcc_dll" }
+	end
 else
 	package.libpaths = { "$(WXWIN)/lib/vc_dll" }
 end
@@ -49,16 +65,16 @@ if ( OS == "windows") then
 	package.bindir = "../../../../bin"
 	package.libdir = "../../../../bin"
 else
-	package.bindir = "../../../../bin/lib"
+	package.bindir = "../../../../bin"
 	package.libdir = "../../../../bin/lib"
 end
 
 -- Set libraries to link.
 if ( OS == "windows") then
-	package.config["Debug"].links = { "wxmsw27d" }
-	package.config["Debug (Unicode)"].links = { "wxmsw27ud" }
-	package.config["Release"].links = { "wxmsw27" }
-	package.config["Release (Unicode)"].links = { "wxmsw27u" }
+	package.config["Debug"].links = { "wxmsw"..wx_ver.."d" }
+	package.config["Debug (Unicode)"].links = { "wxmsw"..wx_ver.."ud" }
+	package.config["Release"].links = {	"wxmsw"..wx_ver }
+	package.config["Release (Unicode)"].links = { "wxmsw"..wx_ver.."u" }
 else
 	package.config["Debug"].linkoptions = { "`wx-config --debug --libs`"}
 	package.config["Release"].linkoptions = { "`wx-config --libs`" }
