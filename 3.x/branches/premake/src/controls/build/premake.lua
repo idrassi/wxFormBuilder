487db1a750f0204b185cb73806ab683949aa0105
rjpcomputing <rjpcomputing@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2006-12-19 13:57:29 +0000

    Created for a safe place to test premake build system.

---
+++ 3.x/branches/premake/src/controls/build/premake.lua
@@ -0,0 +1,22 @@
+project.name = "wxFormBuilder 3rd-Party Controls"
+if ( OS == "windows") then
+	project.configs = { "Debug", "Debug (Unicode)", "Release", "Release (Unicode)" }
+end
+
+--dofile( "../../../premake/wrap_wxconfig.lua" )
+
+-- Path to wx-config.
+--wxconfigPath = ".." .. wxCFG_PATH_SEP .. ".." .. wxCFG_PATH_SEP .. ".." .. wxCFG_PATH_SEP .. "premake" .. wxCFG_PATH_SEP
+-- Create a wxCfg object.
+--cfg = wxCfg:new()
+--cfg:Init( wxconfigPath )
+
+--Test out functionality
+--print( "wxWidgets version = " .. cfg:GetWxVersion() )
+--print( "wxWidgets Libs = " .. cfg:GetLibs() )
+--print( "wxWidgets cflags = " .. cfg:GetCFlags() )
+
+-- Add sdk projects here.
+dopackage( "propgrid" )
+dopackage( "wxFlatNotebook" )
+dopackage( "wxScintilla" )
