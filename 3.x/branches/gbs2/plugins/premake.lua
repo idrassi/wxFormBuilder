932611a0134bb9559241511a7a8b05356b2400ec
rjmyst3 <rjmyst3@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2007-02-23 19:16:04 +0000

    made a copy

---
+++ 3.x/branches/gbs2/plugins/premake.lua
@@ -0,0 +1,10 @@
+project.name = "wxFormBuilder Plugins"
+
+-- Add packages here.
+dopackage( "additional" )
+dopackage( "common" )
+dopackage( "layout" )
+
+-- Add dependency packages here.
+dopackage( "../sdk/plugin_interface" )
+dopackage( "../sdk/tinyxml" )
