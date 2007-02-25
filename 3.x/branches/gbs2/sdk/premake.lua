c8d5ee9654ba03cd20be12049598b03e4152130c
rjmyst3 <rjmyst3@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2007-02-25 01:49:34 +0000

    Deleted 3.x/branches/gbs2

--- 3.x/branches/gbs2/sdk/premake.lua
+++
@@ -1,5 +0,0 @@
-project.name = "wxFormBuilder SDK"
-
--- Add sdk projects here. (This needs to match the directory name not the package name.)
-dopackage( "plugin_interface" )
-dopackage( "tinyxml" )
