3a88313c9c61c110f1c0b8bbcf97afbee0721dad
rjmyst3 <rjmyst3@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2007-11-02 02:57:32 +0000

    allow passing parameters to premake when invoking the shell script

--- 3.x/branches/macbuildsystem/create_build_files.sh
+++ 3.x/branches/macbuildsystem/create_build_files.sh
@@ -9,11 +9,11 @@ if ! wx-config --debug >/dev/null 2>/dev/null; then
 	debug="--disable-wx-debug"
 fi
 
-sdk/premake/premake-linux --target cb-gcc $unicode $debug --with-wx-shared
+sdk/premake/premake-linux --target cb-gcc $unicode $debug --with-wx-shared $1
 echo done...
 echo 
 #
-sdk/premake/premake-linux --target gnu $unicode $debug --with-wx-shared
+sdk/premake/premake-linux --target gnu $unicode $debug --with-wx-shared $1
 echo done...
 echo 
 #
