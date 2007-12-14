01b4f5907c6945b362a1413b2e42a4426f5ef040
rjmyst3 <rjmyst3@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2007-12-14 19:41:43 +0000

    made a copy

---
+++ 3.x/tags/rc4/install/linux/wxfb_export.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+
+# Get output dir from caller
+if [ ! -n "$1" ]
+then
+ echo "Please specify output directory."
+ exit
+else
+ outputDir=$1
+fi  
+
+# Ensure target directories exist  
+if [ ! -d $outputDir ]
+then
+  mkdir $outputDir
+  mkdir $outputDir/bin
+  mkdir $outputDir/share
+  mkdir $outputDir/share/wxformbuilder
+fi
+
+if [ ! -d $outputDir/bin ]
+then
+  mkdir $outputDir/bin
+fi
+  
+if [ ! -d $outputDir/share ]
+then
+  mkdir $outputDir/share
+  mkdir $outputDir/share/wxformbuilder
+fi
+  
+if [ ! -d $outputDir/share/wxformbuilder ]
+then
+  mkdir $outputDir/share/wxformbuilder
+fi
+
+# copy ouput files to target directories  
+cp -R --interactive output/* $outputDir/share/wxformbuilder/
+rm $outputDir/share/wxformbuilder/Changelog.txt
+
+# reorganize target directories
+if [ -d $outputDir/bin ]
+then
+  mv -f $outputDir/share/wxformbuilder/bin/* $outputDir/bin
+  rm -r $outputDir/share/wxformbuilder/bin
+else
+  mv -f $outputDir/share/wxformbuilder/bin $outputDir/
+fi
+
+if [ -d $outputDir/lib ]
+then
+  mv -f $outputDir/share/wxformbuilder/lib/* $outputDir/lib
+  rm - r $outputDir/share/wxformbuilder/lib
+else
+ mv -f $outputDir/share/wxformbuilder/lib $outputDir/
+fi
+
+if [ -d $outputDir/share/wxformbuilder/share ]
+then
+ rm -r $outputDir/share/wxformbuilder/share
+fi
+
+exit
+
---
+++ 3.x/tags/rc4/src/maingui.h
@@ -0,0 +1,24 @@
+#ifndef __MAINGUI__
+#define __MAINGUI__
+
+#include <wx/app.h>
+
+class MyApp : public wxApp
+{
+private:
+  wxLog * m_old_log;
+  wxLogWindow * m_log;
+
+public:
+  bool OnInit();
+  #if wxUSE_ON_FATAL_EXCEPTION && wxUSE_STACKWALKER
+  void OnFatalException();
+  #endif
+  int OnRun();
+  int OnExit();
+  ~MyApp();
+};
+
+DECLARE_APP(MyApp)
+
+#endif //__MAINGUI__
