8a78b779231708ec6244247d8f90a7879e8ccd4e
rjmyst3 <rjmyst3@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2007-12-17 22:08:30 +0000

    made a copy

---
+++ 3.x/tags/rc4-1/src/rad/wxfbmanager.h
@@ -0,0 +1,31 @@
+#ifndef WXFBMANAGER
+#define WXFBMANAGER
+
+#include <component.h>
+#include "utils/wxfbdefs.h"
+
+class VisualEditor;
+class ObjectBase;
+
+class wxFBManager : public IManager
+{
+private:
+	VisualEditor* m_visualEdit;
+
+public:
+	wxFBManager();
+	void SetVisualEditor( VisualEditor* visualEdit );
+	size_t GetChildCount( wxObject* wxobject );
+	wxObject* GetChild( wxObject* wxobject, size_t childIndex );
+	wxObject* GetParent( wxObject* wxobject );
+	IObject* GetIParent( wxObject* wxobject );
+	IObject* GetIObject( wxObject* wxobject );
+	wxObject* GetWxObject( PObjectBase obj );
+	wxNoObject* NewNoObject();
+
+	void ModifyProperty( wxObject* wxobject, wxString property, wxString value, bool allowUndo = true );
+
+	void SelectObject( wxObject* wxobject );
+};
+
+#endif //WXFBMANAGER
