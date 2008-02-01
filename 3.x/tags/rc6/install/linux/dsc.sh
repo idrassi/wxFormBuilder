7b997e961e9a3e21c7b1593a0fb940a020bd3245
rjmyst3 <rjmyst3@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2008-02-01 13:21:19 +0000

    made a copy

---
+++ 3.x/tags/rc6/install/linux/dsc.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+if [ -f wxformbuilder-*.orig.tar.gz ]
+then
+   echo "Please clean old source tarballs first"
+   exit 1
+fi
+
+# get source tarball
+debian/rules get-orig-source
+
+#extract it
+tar xzf wxformbuilder_*.orig.tar.gz
+
+#determine version
+changelog=`cd wxformbuilder_*.orig && dpkg-parsechangelog`
+version=`expr match "$changelog" '.*Version: \([0-9]\.[0-9]\{1,2\}\.[0-9]\+\).*'`
+
+#rename it
+sourcedir=wxformbuilder-$version
+mv wxformbuilder_*.orig $sourcedir
+
+#save current dir
+currentdir=`pwd`
+
+#enter source dir
+cd $sourcedir
+
+#make debian source archive
+dpkg-buildpackage -S -sa -rfakeroot
+
+#cleanup
+cd $currentdir
+rm -r $sourcedir
