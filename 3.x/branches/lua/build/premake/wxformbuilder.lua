a5d82518541f9f29594c4ef6ca7ff72a7ee073f2
michalbliznak <michalbliznak@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2012-12-12 08:08:16 +0000

    - Created branch for implementation of wxLua support.

---
+++ 3.x/branches/lua/build/premake/wxformbuilder.lua
@@ -0,0 +1,90 @@
+-----------------------------------------------------------------------------
+--  Name:        wxformbuilder.lua
+--  Purpose:     Main application project
+--  Author:      Andrea Zanellato
+--  Modified by: 
+--  Created:     19/10/2011
+--  Copyright:   (c) 2011 wxFormBuilder Team
+--  Licence:     GNU General Public License Version 2
+-----------------------------------------------------------------------------
+project "wxFormBuilder"
+    kind                    "WindowedApp"
+    files
+    {
+        "../../src/**.h", "../../src/**.hpp", "../../src/**.hh",
+        "../../src/**.cpp", "../../src/**.cc", "../../src/**.fbp"
+    }
+    excludes                { "../../src/controls/**" }
+    includedirs
+    {
+        "../../src", "../../src/boost", "../../src/controls/include",
+        "../../sdk/tinyxml", "../../sdk/plugin_interface"
+    }
+    defines                 {"NO_GCC_PRAGMA", "TIXML_USE_TICPP", "APPEND_WXVERSION"}
+    flags                   {"ExtraWarnings"}
+    libdirs                 {"../../sdk/lib"}
+    links                   {"TiCPP", "plugin-interface"}
+
+    local libs = ""
+if wxVersion < "2.9" then
+	defines					{"USE_FLATNOTEBOOK"}
+    links                   {"wxPropertyGrid", "wxScintilla", "wxFlatNotebook"}
+else
+    excludes
+    {
+        "../../src/controls/include/wx/propgrid/**.h",
+        "../../src/controls/include/wx/wxScintilla/**.h",
+        "../../src/controls/include/wx/wxFlatNotebook/**.h",
+        "../../src/controls/src/propgrid/**.cpp",
+        "../../src/controls/src/wxScintilla/**.cpp",
+        "../../src/controls/src/wxFlatNotebook/**.cpp"
+    }
+    libs                    = "all"
+end
+
+	if wxArchitecture then
+		buildoptions	{"-arch " .. wxArchitecture}
+	end
+
+	configuration {"codelite", "linux"}
+        linkoptions         {"-Wl,-rpath,$$``ORIGIN/../lib/wxformbuilder"}
+
+    configuration {"codeblocks", "linux"}
+        linkoptions         {"-Wl,-rpath,$``ORIGIN/../lib/wxformbuilder"}
+
+    configuration "macosx"
+        linkoptions         {"-Wl,-L../../output/lib/wxformbuilder"}
+
+    configuration {"macosx", "Debug"}
+        postbuildcommands   {"../../../install/macosx/postbuildd4.sh"}
+
+    configuration {"macosx", "Release"}
+        postbuildcommands   {"../../../install/macosx/postbuild4.sh"}
+
+    configuration "not windows"
+        excludes            {"../../src/*.rc"}
+        libdirs             {"../../output/lib/wxformbuilder"}
+        targetdir           "../../output/bin"
+        targetname          "wxformbuilder"
+
+    configuration "windows"
+        files               {"../../src/*.rc"}
+        libdirs             {"../../output"}
+        targetdir           "../../output"
+
+if wxCompiler == "gcc" then
+        buildoptions        {"-gstabs"}
+        links               {"bfd", "intl", "iberty", "psapi", "imagehlp"}
+end
+    configuration "Debug"
+        defines             {"__WXFB_DEBUG__"}
+        targetsuffix        ( DebugSuffix )
+        wx_config           { Libs=libs, Debug="yes" }
+
+    configuration "Release"
+        buildoptions        {"-fno-strict-aliasing"}
+        wx_config           { Libs=libs }
+
+    if os.is("windows") then
+        flags               {"Symbols"}
+    end
---
+++ 3.x/branches/lua/create_build_files4.sh
@@ -0,0 +1,92 @@
+#!/bin/sh
+
+# Parse command line options
+shared=""
+arch=""
+wxroot=""
+wxpath=`wx-config --prefix`
+
+# These works only on wxWidgets 2.8.10+
+#wxcharset=`wx-config --query-chartype`
+#wxversion=`wx-config --query-version`
+for args in "$@"
+do
+    haveroot=`expr "${args}" : '--wx-root=.*'`
+	havearch=`expr "${args}" : '--architecture=.*'`
+    if ( [ ${args} = "--help" ] || [ ${args} = "-h" ] ); then
+			echo "Available options:"
+			echo
+			echo "--disable-mediactrl       Disable wxMediaCtrl / wxMedia library."
+			echo
+			echo "--disable-shared			 Use static wxWidgets build instead of shared libraries."
+			echo
+			echo "--disable-unicode         Whether to use an Unicode or an ANSI build."
+			echo "                          Ignored in wxWidgets 2.9 and later."
+			echo "                          Example: --disable-unicode produces an ANSI build."
+			echo "                          Default: Unicode build on all versions."
+			#       echo "                          Current: $wxcharset"
+			echo
+			echo "--wx-root                 Specify the wxWidgets build path,"
+			echo "                          useful for wxWidgets builds not installed"
+			echo "                          in your system (alternate/custom builds)"
+			echo "                          Example: --wx-root=/home/devel/wx/3.0/buildgtk"
+			echo "                          Current: $wxpath"
+			echo
+			echo "--architecture			 Specify build architecture (e.g. --architecture=i386)."
+			echo
+			exit
+    elif [ ${args} = "--disable-mediactrl" ]; then
+        mediactrl="--disable-mediactrl"
+        continue
+    elif [ ${args} = "--disable-unicode" ]; then
+        wxunicode="--disable-unicode"
+        continue
+	elif [ ${args} = "--disable-shared" ]; then
+        shared="--disable-shared"
+        continue
+	elif [ ${args} = "--disable-unicode" ]; then
+        wxunicode="--disable-unicode"
+        continue
+    elif ( [ "$haveroot" -gt "0" ] ); then
+        wxroot=${args}
+        continue
+	elif ( [ "$havearch" -gt "0" ] ); then
+        arch=${args}
+        continue
+    fi
+done
+
+# Autodetect wxWidgets version
+if [ "$wxroot" = "" ]; then
+    wxver=`wx-config --release`
+else
+    wxpath=${wxroot#-*=}
+    wxver=`$wxpath/wx-config --release`
+fi
+
+wxversion="--wx-version="$wxver
+
+# Autodetect OS
+isbsd=`expr "$unamestr" : '.*BSD'`
+platform="unknown"
+unamestr=$(uname)
+
+if ( [ "$isbsd" -gt "0" ] ); then
+    platform="bsd"
+elif [ "$unamestr" = "Linux" ]; then
+    platform="linux"
+elif [ "$unamestr" = "Darwin" ]; then
+    platform="macosx"
+fi
+
+# Build premake
+cd build
+make CONFIG=Release -C./premake/$platform
+
+#./premake/$platform/bin/release/premake4 --file=./premake/solution.lua $wxunicode $wxroot $wxversion $mediactrl $shared $arch codeblocks
+./premake/$platform/bin/release/premake4 --file=./premake/solution.lua $wxunicode $wxroot $wxversion $mediactrl $shared $arch codelite
+./premake/$platform/bin/release/premake4 --file=./premake/solution.lua $wxunicode $wxroot $wxversion $mediactrl $shared $arch gmake
+if [ "$platform" = "macosx" ]; then
+   ./premake/$platform/bin/release/premake4 --file=./premake/solution.lua $wxunicode $wxroot $wxversion $mediactrl $shared $arch xcode3
+fi
+
