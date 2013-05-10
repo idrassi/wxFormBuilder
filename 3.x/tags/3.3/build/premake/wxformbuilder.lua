dee9b706088194b3d30c1d40d505cf2130c9dc11
michalbliznak <michalbliznak@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2013-05-10 15:43:46 +0000

    - Created tag for 3.3 version.

---
+++ 3.x/tags/3.3/build/premake/wxformbuilder.lua
@@ -0,0 +1,116 @@
+-----------------------------------------------------------------------------
+--  Name:        wxformbuilder.lua
+--  Purpose:     Main application project
+--  Author:      Andrea Zanellato
+--  Modified by: 
+--  Created:     19/10/2011
+--  Copyright:   (c) 2011 wxFormBuilder Team
+--  Licence:     GNU General Public License Version 2
+-----------------------------------------------------------------------------
+project "wxFormBuilder"
+    kind                    "WindowedApp"
+    files
+    {
+        "../../src/**.h", "../../src/**.hpp", "../../src/**.hh",
+        "../../src/**.cpp", "../../src/**.cc", "../../src/**.fbp"
+    }
+    excludes
+	{
+		"../../src/controls/**",
+		"../../src/rad/designer/resizablepanel.*"
+	}
+    includedirs
+    {
+        "../../src", "../../src/boost", "../../src/controls/include",
+        "../../sdk/tinyxml", "../../sdk/plugin_interface"
+    }
+    defines                 {"NO_GCC_PRAGMA", "TIXML_USE_TICPP", "APPEND_WXVERSION"}
+    flags                   {"ExtraWarnings"}
+    libdirs                 {"../../sdk/lib"}
+    links                   {"TiCPP", "plugin-interface"}
+
+    local libs = ""
+if wxVersion < "2.9" then
+	defines					{"USE_FLATNOTEBOOK"}
+    links                   {"wxPropertyGrid", "wxScintilla", "wxFlatNotebook"}
+else
+    excludes
+    {
+        "../../src/controls/include/wx/propgrid/**.h",
+        "../../src/controls/include/wx/wxScintilla/**.h",
+        "../../src/controls/include/wx/wxFlatNotebook/**.h",
+        "../../src/controls/src/propgrid/**.cpp",
+        "../../src/controls/src/wxScintilla/**.cpp",
+        "../../src/controls/src/wxFlatNotebook/**.cpp"
+    }
+    libs                    = "all"
+end
+
+	if wxArchitecture then
+		buildoptions	{"-arch " .. wxArchitecture}
+	end
+	
+	if os.is( "linux" ) then
+		newoption
+		{
+			trigger		= "rpath",
+			description	= "Linux only, set rpath on the linker line to find shared libraries next to executable"
+		}
+
+		-- Set rpath
+		local useRpath = true
+		local rpath= _ACTION == "codeblocks" and "$" or "$$"
+		rpath = rpath .. "``ORIGIN/../lib/wxformbuilder"
+		local rpathOption = _OPTIONS[ "rpath" ]
+
+		if rpathOption then
+			if "no" == rpathOption or "" == rpathOption then
+				useRpath = false
+			else
+				rpath = rpathOption
+			end
+		end
+
+		if useRpath then
+		print( "rpath: -Wl,-rpath," .. rpath )
+			linkoptions( "-Wl,-rpath," .. rpath )
+		end
+	end
+	
+    configuration "macosx"
+        linkoptions         {"-Wl,-L../../output/lib/wxformbuilder"}
+
+    configuration {"macosx", "Debug"}
+        postbuildcommands   {"../../../install/macosx/postbuildd4.sh"}
+
+    configuration {"macosx", "Release"}
+        postbuildcommands   {"../../../install/macosx/postbuild4.sh"}
+
+    configuration "not windows"
+        excludes            {"../../src/*.rc"}
+        libdirs             {"../../output/lib/wxformbuilder"}
+        targetdir           "../../output/bin"
+        targetname          "wxformbuilder"
+        links               {"dl"}
+
+    configuration "windows"
+        files               {"../../src/*.rc"}
+        libdirs             {"../../output"}
+        targetdir           "../../output"
+
+if wxCompiler == "gcc" then
+        buildoptions        {"-gstabs"}
+        links               {"bfd", "intl", "iberty", "psapi", "imagehlp"}
+end
+    configuration "Debug"
+        defines             {"__WXFB_DEBUG__"}
+        targetsuffix        ( DebugSuffix )
+        wx_config           { Libs=libs, Debug="yes" }
+
+    configuration "Release"
+        buildoptions        {"-fno-strict-aliasing"}
+        wx_config           { Libs=libs }
+
+    if os.is("windows") then
+        flags               {"Symbols"}
+    end
---
+++ 3.x/tags/3.3/plugins/premake.lua
@@ -0,0 +1,13 @@
+project.name = "wxFormBuilder Plugins"
+
+-- Add packages here.
+dopackage( "additional" )
+dopackage( "common" )
+dopackage( "containers" )
+dopackage( "forms" )
+dopackage( "layout" )
+dopackage( "wxAdditions" )
+
+-- Add dependency packages here.
+dopackage( "../sdk/plugin_interface" )
+dopackage( "../sdk/tinyxml" )
