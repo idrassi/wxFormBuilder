7ed23c8ae2b94223f2a5d9a005491b9aca57426d
csteenwyk <csteenwyk@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2013-11-20 20:06:35 +0000

    Fixed premake, added temp make batch file

---
+++ plugins/additions/branches/wx30/wxfbPlugin/make.bat
@@ -0,0 +1,1 @@
+..\build\premake\windows\premake4.exe --targetdir-base=./ --compiler-version=48 --wx-root=C:\Users\chris.steenwyk\Documents\GitHub\wxpack-1\wxwidgets --wx-version=30 --wx-shared --monolithic --unicode codelite
