00a8744c52b307b2d2e3ad46306f82516b7d1eed
csteenwyk <csteenwyk@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2013-11-20 22:12:56 +0000

    Created build batch file

--- plugins/additions/branches/wx30/wxfbPlugin/make.bat
+++
@@ -1,1 +0,0 @@
-..\build\premake\windows\premake4.exe --targetdir-base=./ --compiler-version=48 --wx-root=C:\Users\chris.steenwyk\Documents\GitHub\wxpack-1\wxwidgets --wx-version=30 --wx-shared --monolithic --unicode codelite
